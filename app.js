41b7966a01fccab3bb5ace026eee97ec7a0097fe
javlon <Javlon2003@gmail.com>
2022-07-11 21:00:43 +0300

    My self

---
+++ app.js
@@ -0,0 +1,5 @@
+// let themeColor =document.querySelectorAll('.theme-toggler span');
+// themeColor.forEach(color=>color.addEventListener('click',()=>{
+//     let backround=color.getElementsByClassName.backround;
+//     document.querySelector('body').style.background=backround;
+// }));
